17ab8b7bf770b5ccc1d51d192ecb0d30ba425f5c
Anthony K <antonyk@users.noreply.github.com>
2020-03-12 19:08:08 -0400

    generic element maker function; card maker function; init axios client; some debug code

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,37 @@
+// Reusables:
+function elementMaker(elementType, elementText = '', elementClass = '') {
+  if (!elementType) throw "Missing element type parameter!";
+  try {
+    let e = document.createElement(elementType);
+    if (elementText) e.textContent = elementText;
+    if (elementClass) e.classList.add(elementClass);
+    return e;
+  }
+  catch(e) {
+    throw "Invalid element type!";
+  } 
+}
+
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// create a new axios HTTP client instance and configure default request url
+
+const github = axios.create({
+  baseURL: 'https://api.github.com'
+});
+
+// make a GET request using a relative uri path
+// github.get('/antonyk')
+// .then(response => {
+//   document.querySelector('.test').append(elementMaker('p', response.data));
+// })
+// .catch(error => {
+//   document.querySelector('.test').append(elementMaker('p', 'ERROR: ' + error.toJSON()));
+//   // console.log(error.toJSON());
+// });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -26,8 +56,38 @@
 
 const followersArray = [];
 
+function createCard(data) {
+
+  // element = elementMaker(type [, text, class])
+  let card = elementMaker('div', '', 'card');
+
+  let img = elementMaker('img');
+  img.src = data.image;
+
+  let info = elementMaker('div', '', 'card-info');
+  let name = elementMaker('h3', data.name, 'name');
+  let uname = elementMaker('p', data.uname, 'username');
+  let loc = elementMaker('p', `Location: ${data.location}`);
+  let prof = elementMaker('p', `Profile: `);
+  let profLink = elementMaker('a', data.url);
+  let followers = elementMaker('p', `Followers: ${data.followers}`);
+  let following = elementMaker('p', `Following: ${data.following}`);
+  let bio = elementMaker('p', `Bio: ${data.bio}`);
+
+  // nest
+  
+
+
+  card = document.createElement('div');
+  card.classList.add('card');
+
+}
+
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
+
+
+
 
 <div class="card">
   <img src={image url of user} />
